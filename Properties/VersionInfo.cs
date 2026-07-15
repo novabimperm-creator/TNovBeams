3def33f75aa904d5b95e701da151a3a71dc1618a
novabimperm-creator <roshior.a@pm-nova.ru>
2026-07-15 16:03:28 +0500

    new UI release

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.26.0714.1036")]
-[assembly: AssemblyFileVersion("1.26.0714.1036")]
+[assembly: AssemblyVersion("1.26.0715.927")]
+[assembly: AssemblyFileVersion("1.26.0715.927")]
